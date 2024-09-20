263386a40ff83e704f38ba0e335864aa9f5e8bd7
Kentent Roth <kentencroth@yahoo.com>
2024-09-20 16:07:02 -0500

    create game added

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { AppDispatch } from '../app/store';
 import { fetchMyGames } from '../features/gameSlice';
@@ -6,14 +6,25 @@ import { useDispatch } from 'react-redux';
 
 const Home = () => {
 	const dispatch = useDispatch<AppDispatch>();
+	const [name, setName] = useState('');
 
 	useEffect(() => {
 		dispatch(fetchMyGames());
-	});
+	}, []);
+
+	const sendData = () => {
+		console.log(name);
+	};
 
 	return (
 		<>
 			<h1>Home</h1>
+			<input
+				type="text"
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+			/>
+			<button onClick={sendData}>Send</button>
 		</>
 	);
 };
