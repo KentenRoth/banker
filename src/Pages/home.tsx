94b8f74c8bece84461420e4148f4a7d2f9362b46
Kentent Roth <kentencroth@yahoo.com>
2024-09-21 10:46:06 -0500

    Ability to create new game

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -3,17 +3,27 @@ import { useEffect, useState } from 'react';
 import { AppDispatch } from '../app/store';
 import { fetchMyGames } from '../features/gameSlice';
 import { useDispatch } from 'react-redux';
+import useSendData from '../hooks/sendData';
+import { createGame } from '../features/gameSlice';
 
 const Home = () => {
 	const dispatch = useDispatch<AppDispatch>();
 	const [name, setName] = useState('');
+	const { sendData, loading, error } = useSendData('/games');
 
 	useEffect(() => {
 		dispatch(fetchMyGames());
 	}, []);
 
-	const sendData = () => {
-		console.log(name);
+	const newGame = async () => {
+		try {
+			const response = await sendData({ name });
+			if (response.status === 200) {
+				dispatch(createGame(response.data));
+			}
+		} catch (error) {
+			console.error(error);
+		}
 	};
 
 	return (
@@ -24,7 +34,7 @@ const Home = () => {
 				value={name}
 				onChange={(e) => setName(e.target.value)}
 			/>
-			<button onClick={sendData}>Send</button>
+			<button onClick={newGame}>Send</button>
 		</>
 	);
 };
